fdefc6b8357ff2be994b21281cde303d48da9a74
Maria Gabriela Lopez <47748533+magalopez@users.noreply.github.com>
2022-10-18 15:35:55 -0500

    update

--- src/avos.js
+++ src/avos.js
@@ -29,47 +29,47 @@ function formatPrice (price) {
 //web api
 //Connect to Server
 //promise -> async/await
-const getData = async (baseUrl, url) => {
-  const response = await fetch(`${baseUrl}${url}`);
-  const responseJson = await response.json();
-  return responseJson.data;
-}
+// const getData = async (baseUrl, url) => {
+//   const response = await fetch(`${baseUrl}${url}`);
+//   const responseJson = await response.json();
+//   return responseJson.data;
+// }
 
-const data = await getData(baseUrl, url);
+// const data = await getData(baseUrl, url);
 
-const cards = [];
+// const cards = [];
 
-data.forEach(item => {
-  //image
-  const image = document.createElement('img');
-  document.body.appendChild(image);
-  image.setAttribute('src', `${baseUrl}${item.image}`);
-  image.className = "w-24 rounded-full";
+// data.forEach(item => {
+//   //image
+//   const image = document.createElement('img');
+//   document.body.appendChild(image);
+//   image.setAttribute('src', `${baseUrl}${item.image}`);
+//   image.className = "w-24 rounded-full";
 
-  //title
-  const title = document.createElement('h2');
-  document.body.appendChild(title);
-  title.innerText = item.name;
-  // title.style = "font-size: 2rem"
-  // title.style.fontSize = "3rem"
-  title.className = "text-base";
+//   //title
+//   const title = document.createElement('h2');
+//   document.body.appendChild(title);
+//   title.innerText = item.name;
+//   // title.style = "font-size: 2rem"
+//   // title.style.fontSize = "3rem"
+//   title.className = "text-base";
 
-  //price
-  const price = document.createElement('div')
-  price.innerText = formatPrice(item.price);
-  // price.className = "text-gray-600";
+//   //price
+//   const price = document.createElement('div')
+//   price.innerText = formatPrice(item.price);
+//   // price.className = "text-gray-600";
 
-  const wrapperRight = document.createElement('div');
-  wrapperRight.append(title, price);
-  wrapperRight.className = "flex flex-col items-start justify-center p-4";
+//   const wrapperRight = document.createElement('div');
+//   wrapperRight.append(title, price);
+//   wrapperRight.className = "flex flex-col items-start justify-center p-4";
 
-  const container = document.createElement('div');
-  container.append(image, wrapperRight);
-  container.className = "flex flex-row bg-gray-200 rounded-xl p-4 hover:bg-gray-500 hover:text-white cursor-pointer"
-  cards.push(container);
-})
+//   const container = document.createElement('div');
+//   container.append(image, wrapperRight);
+//   container.className = "flex flex-row bg-gray-200 rounded-xl p-4 hover:bg-gray-500 hover:text-white cursor-pointer"
+//   cards.push(container);
+// })
 
-avoNode.className = "container mx-auto grid gap-4 grid-cols-3 grid-rows-3 mt-8";
-avoNode.append(...cards);
+// avoNode.className = "container mx-auto grid gap-4 grid-cols-3 grid-rows-3 mt-8";
+// avoNode.append(...cards);
 
 console.log('Happy ending :)');
--- src/lazy.js
+++ src/lazy.js
@@ -1,21 +1,37 @@
-function isIntersecting (entry) {
-	return entry.isIntersecting;
-};
-
-function loadImage (entry) {
-	const container = entry.target;
-	const skeletonImage = container.firstChild;
-	const image = skeletonImage.firstChild;
-	const url = image.dataset.src;
-	image.src = url;
-
-	observer.unobserve(container)
-};
+// import { totalImages, loadedImages } from './foxs';
+let totalImages = 0;
+let loadedImages = 0;
 
 const observer = new IntersectionObserver((entries) => {
 	entries.filter(isIntersecting).forEach(loadImage)
 });
 
-export function registerImage (image) {
-	observer.observe(image)
+const isIntersecting = intersectionEntry => intersectionEntry.isIntersecting;
+
+function loadImage (intersectionEntry) {
+	const container = intersectionEntry.target;
+	const skeletonImage = container.firstChild;
+	const image = skeletonImage.firstChild;
+	const url = image.dataset.src;
+	image.src = url;
+
+	image.onload = () => {
+		loadedImages += 1;
+		logState();
+	}
+
+	observer.unobserve(container)
 };
+
+
+export function registerImage (image) {
+	observer.observe(image);
+	totalImages += 1;
+  logState();
+};
+
+function logState() {
+  console.log(`⚪️ Total Imágenes: ${totalImages}`);
+  console.log(`🟣 Imágenes cargadas: ${loadedImages}`);
+  console.log("--------------------------------------");
+}
