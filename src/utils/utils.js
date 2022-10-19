5cc0d5f659f18853bfd6107612a0c9f3bb3ac23a
magabriela <magalopeap@gmail.com>
2022-10-18 21:02:56 -0500

    feat: add hyperscript

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,24 +1,22 @@
+import hyperscript from 'hyperscript';
+
 const minimum = 1;
 const maximum = 122;
 
 const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
 
 export const createImageNode = baseUrl => {
-	const imageContainerHTML = document.createElement('div');
-	imageContainerHTML.className = "p-4";
+	const imageHTML = hyperscript('img.mx-auto.rounded-xl', {
+		"width" : "350",
+		"data-src" : `${baseUrl}${random()}.jpg`
+	})
 
-	const skeletonImageHTML = document.createElement('div');
-	skeletonImageHTML.style.width = "350px";
-	skeletonImageHTML.style.height = "auto";
-	skeletonImageHTML.className = "bg-gray-100 mx-auto rounded-xl";
+	const skeletonImageHTML = hyperscript('div.bg-gray-100.mx-auto.rounded-xl', {
+		"width" : "350px",
+		"height": "auto"
+	}, imageHTML)
 
-	const imageHTML = document.createElement('img');
-	imageHTML.dataset.src = `${baseUrl}${random()}.jpg`;
-	imageHTML.width = 350;
-	imageHTML.className = "mx-auto rounded-xl";
-
-	skeletonImageHTML.appendChild(imageHTML)
-	imageContainerHTML.appendChild(skeletonImageHTML);
+	const imageContainerHTML =  hyperscript('div.p-4', skeletonImageHTML);
 
 	return imageContainerHTML;
 };
